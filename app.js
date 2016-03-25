b55b2ffbe23d8e66e312bedebf13d0cbe81f3fef
asifaly <asifaly82@yahoo.com>
2016-03-25 19:57:29 +0530

    trooper collission detect added

--- app.js
+++ app.js
@@ -3,25 +3,36 @@
 $(document).ready(function() {
 
     var trooperId = 0;
-
+    var troopers = {};
     var game = {
+
+        init: function(){
+          this.podPosition = 0;
+          this.podIncrement = 10;
+          this.trooperCreateSpeed = 1000;
+          this.trooperFallSpeed = 10;
+          this.trooperInit();
+        },
+
         podRight: function() {
             var pos = parseInt($("#pod").css('left'));
-            console.log(pos);
-            $("#pod").css('left', pos + 10 + 'px');
+            this.podPosition = pos + 10;
+            $("#pod").css('left', this.podPosition + 'px');
         },
         podLeft: function() {
             var pos = parseInt($("#pod").css('left'));
-            $("#pod").css('left', pos - 10 + 'px');
+            this.podPosition = pos - 10;
+            $("#pod").css('left', this.podPosition + 'px');
         },
         trooperInit: function() {
 
-            this.trooperInitInterval = setInterval(createTrooper, 2000);
+            this.trooperInitInterval = setInterval(createTrooper, this.trooperCreateSpeed);
 
             function createTrooper() {
                 trooperId++;
                 var trooperPos = Math.floor(Math.random() * 420) + 10;
                 var newTrooper = '<div class="troopers" id=' + trooperId + '></div>';
+                troopers[trooperId] = trooperPos;
                 $(".container").append(newTrooper);
                 $("#" + trooperId).css('left', trooperPos + 'px');
                 game.trooperFall(trooperId);
@@ -29,25 +40,33 @@ $(document).ready(function() {
         },
 
         trooperFall: function(trooper) {
-
-            var trooperFallInterval = setInterval(fall, 10);
-            console.log(trooperFallInterval);
+            var _this = this;
+            var trooperFallInterval = setInterval(fall, this.trooperFallSpeed);
             var top = parseInt($("#" + trooper).css('top'));
-            //$('#'+trooper).attr('id',trooperFallInterval);
-            function fall() {
 
-                if (top < 330) {
-                    top++;
-                    $("#" + trooper).css('top', top + 'px');
-                } else {
+            function fall() {
+                top++;
+
+                if (top === 295 && troopers[trooper] >= _this.podPosition && troopers[trooper] <= _this.podPosition + 75 - 20) {
+                    console.log("collission");
+                    console.log(trooper);
+                    console.log(trooperFallInterval);
                     clearInterval(trooperFallInterval);
-                    $("#"+trooper).hide("slow");
+
+                } else if (top < 330) {
+
+                    $("#" + trooper).css('top', top + 'px');
+
+                } else {
+
+                    clearInterval(trooperFallInterval);
+                    $("#" + trooper).hide("slow");
                 }
             }
 
         },
 
-        detectCollission: function(trooper) {
+        checkCollission: function() {
 
         }
 
@@ -64,16 +83,12 @@ $(document).ready(function() {
                 game.podRight();
                 break;
 
-            case 32: //space
-                game.stop();
-                break;
-
             default:
                 return; // exit this handler for other keys
         }
         e.preventDefault();
     });
 
-    game.trooperInit();
+    game.init();
 
 });
