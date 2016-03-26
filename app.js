ae13f1ae10d03ba11c09594d93c3b7f9e1c2127e
asifaly <asifaly82@yahoo.com>
2016-03-26 13:29:10 +0530

    created separate class for falling object, added initial powerup and danger logic

--- app.js
+++ app.js
@@ -1,100 +1,130 @@
 //Math.floor(Math.random() * 430) + 10 
-
+//begin game
 $(document).ready(function () {
-
-  var trooperId = 0;
-  var troopers = {};
-  var game = {
-
-    init: function () {
-      this.podPosition = 0;
-      this.podSpeed = 50;
-      this.trooperCreateSpeed = 3000;
-      this.trooperFallSpeed = 20;
-      this.trooperInit();
-    },
-
-    podRight: function () {
-      var pos = parseInt($("#pod").css('left'));
-      this.podPosition = pos + this.podSpeed;
-      $("#pod").css('left', this.podPosition + 'px');
-    },
-    podLeft: function () {
-      var pos = parseInt($("#pod").css('left'));
-      this.podPosition = pos - this.podSpeed;
-      $("#pod").css('left', this.podPosition + 'px');
-    },
-    trooperInit: function () {
-
-      this.trooperInitInterval = setInterval(createTrooper, this.trooperCreateSpeed);
-
-      function createTrooper() {
-        trooperId++;
-        var trooperPos = Math.floor(Math.random() * 420) + 10;
-        var newTrooper = '<div class="troopers" id=' + trooperId + '></div>';
-        troopers["#"+trooperId] = trooperPos;
-        $(".container").append(newTrooper);
-        $("#" + trooperId).css('left', trooperPos + 'px');
-        game.trooperFall("#"+trooperId);
-      }
-    },
-
-    trooperFall: function (trooper) {
-      var _this = this;
-      var trooperFallInterval = setInterval(fall, this.trooperFallSpeed);
-      var top = parseInt($(trooper).css('top'));
-
-      function fall() {
-        top++;
-
-        if (top === 285 && troopers[trooper] + 10 >= _this.podPosition && troopers[trooper] <= _this.podPosition + 105 - 30) {
-          clearInterval(trooperFallInterval);
-          // $(trooper).addClass("alive");
-          setTimeout(function () {
-            $(trooper).remove();
-          }, 1000);
-
-
-        } else if (top < 320) {
-
-          $(trooper).css('top', top + 'px');
-
-        } else {
-
-          clearInterval(trooperFallInterval);
-          trooper % 2 === 0 ? $(trooper).addClass("dead1") : $(trooper).addClass("dead2");
-          setTimeout(function () {
-            $(trooper).remove();
-          }, 1000);
-        }
-
-      }
-
-    },
-
-    checkCollission: function () {
-
-    },
-
-
-  };
-
-  $(document).keydown(function (e) {
-    switch (e.which) {
-    case 37: // left
-      game.podLeft();
-      break;
-
-    case 39: // right
-      game.podRight();
-      break;
-
-    default:
-      return; // exit this handler for other keys
-    }
-    e.preventDefault();
-  });
-
   game.init();
+});
 
+//define the pod object
+var pod = {
+  //initialize pod speed and position
+  init: function () {
+    this.podPosition = 0;
+    this.podSpeed = 50;
+    this.pod = $("#pod");
+  },
+  //move pod to the right
+  moveRight: function () {
+    var pos = parseInt(this.pod.css('left'));
+    this.podPosition = pos + this.podSpeed;
+    this.pod.css('left', this.podPosition + 'px');
+  },
+  //move pod to the left
+  moveLeft: function () {
+    var pos = parseInt(this.pod.css('left'));
+    this.podPosition = pos - this.podSpeed;
+    this.pod.css('left', this.podPosition + 'px');
+  },
+  //return current podposition i.e left of the pod
+  getPosition: function () {
+    return this.podPosition;
+  }
+};
+
+//generic falling object the argument x will contain the style to be applied trooper powerup etc.
+var FallingObject = function (x) {
+  this.type = x;
+  this.id = Date.now();
+  this.html = '<div class="' + this.type + '" id=' + this.id + '></div>';
+  this.fallSpeed = Math.floor(Math.random() * 50) + 10;
+  this.pos = Math.floor(Math.random() * 420) + 10;
+  console.log(this);
+  $(".container").append(this.html);
+  $("#" + this.id).css('left', this.pos + 'px');
+};
+
+//game object to control the game play
+var game = {
+  //initialize the game by initializing pod and troopercreate speed and being trooper creation
+  init: function () {
+    pod.init();
+    this.trooperCreateSpeed = 3000;
+    this.powerCreateSpeed = 20000;
+    this.dangerCreateSpeed = 30000;
+    this.trooperInit();
+    this.powerUpInit();
+    this.dangerInit();
+  },
+  //create troopers
+  trooperInit: function () {
+    this.trooperInitInterval = setInterval(createTrooper, this.trooperCreateSpeed);
+
+    function createTrooper() {
+      trooper = new FallingObject("trooper");
+      game.startFall(trooper);
+    }
+  },
+
+  powerUpInit: function (){
+    this.powerInitInterval = setInterval(createPower, this.powerCreateSpeed);
+
+    function createPower() {
+      power = new FallingObject("powerup");
+      game.startFall(power);
+    }
+  },
+
+  dangerInit: function (){
+    this.dangerInitInterval = setInterval(createPower, this.dangerCreateSpeed);
+
+    function createPower() {
+      danger = new FallingObject("danger");
+      game.startFall(danger);
+    }
+  },
+  //being fall of created items
+  startFall: function (elem) {
+    var _this = this;
+    elem.FallInterval = setInterval(fall, elem.fallSpeed);
+    var top = parseInt($("#" + elem.id).css('top'));
+
+    function fall() {
+      top++;
+      if (top === 285 && elem.pos + 10 >= pod.getPosition() && elem.pos <= pod.getPosition() + 105 - 30) {
+        clearInterval(elem.FallInterval);
+        // $(elem.id).addClass("alive");
+        _this.removeObject("#"+elem.id);
+      } else if (top < 320) {
+        $("#" + elem.id).css('top', top + 'px');
+      } else {
+        clearInterval(elem.FallInterval);
+        elem.id % 2 === 0 ? $("#" + elem.id).addClass("dead1") : $("#" + elem.id).addClass("dead2");
+        _this.removeObject("#"+elem.id);
+      }
+    }
+  },
+  //need to sepearte the collission function
+  checkCollission: function () {},
+
+  removeObject: function (elem) {
+    setTimeout(function () {
+      $(elem).remove();
+    }, 1000);
+  }
+};
+
+//check for keypress
+$(document).keydown(function (e) {
+  switch (e.which) {
+  case 37: // left
+    pod.moveLeft();
+    break;
+
+  case 39: // right
+    pod.moveRight();
+    break;
+
+  default:
+    return; // exit this handler for other keys
+  }
+  e.preventDefault();
 });
